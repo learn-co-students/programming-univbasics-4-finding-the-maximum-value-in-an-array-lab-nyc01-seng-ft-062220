e1554018b7b87eac391980ed9cb9e1ebd8c35831
Cathy D'Onofrio <catd825@gmail.com>
2020-05-05 00:59:17 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,15 @@
+#def find_max_value(array)
+ # array.max
+#end
+
 def find_max_value(array)
-  # Add your solution here
+  count = 0 #start loop at zero
+  max_value = -1 #assign lowest possible value
+    while count < array.length do #loop over whole array
+      if max_value < array[count] #starts at -1, which will always be the least.  becomes value reassigned below only if value is less than element.
+        max_value = array[count] #new variable is reassigned to new element, and holds at this value until the next loop
+      end
+    count += 1
+  end
+  max_value
 end
