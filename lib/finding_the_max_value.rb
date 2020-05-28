60de221234d35cb4df3bdd192b8005486a020f4b
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-28 11:55:19 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  # Add your solution here
+  largest_number = 0
+  for item in array do
+    if item > largest_number
+      largest_number = item
+    end
+  end
+  return largest_number
 end
